7e8d2e4368add7e990559017565db61c1398328f
Trottatigno <trotta.alexandre@gmail>
2024-08-24 17:26:00 +0200

    création du serveur et DB connectée, mongoose, express, dotenv, nodemon

---
+++ backend/app.js
@@ -0,0 +1,23 @@
+import "dotenv/config";
+import express from "express";
+import mongoose from "mongoose";
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+const DB_URL = process.env.DB_URL;
+
+mongoose
+  .connect(DB_URL)
+  .then(() => {
+    console.log("Connecté à la DB");
+    app.listen(PORT, () => {
+      console.log(`Le serveur est lancé sur le port: 5000`);
+    });
+  })
+  .catch((err) => {
+    console.log(`PAS connecté à la DB`, { err });
+  });
+
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
---
+++ backend/server.js
@@ -0,0 +1,10 @@
+import express from "express";
+const app = express();
+
+PORT = 3000;
+
+app.get()
+
+app.listen(PORT, () => {
+    console.log(`Le serveur est lancé sur le port` {PORT})
+})
